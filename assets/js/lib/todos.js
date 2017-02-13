62983d0534dfbe0be5663d1a6219d4055899e066
Mariusz Brzostek <mariusz.brzostek@gmail.com>
2017-02-13 02:33:14 +0100

    ToDo list end ?

--- assets/js/lib/todos.js
+++ assets/js/lib/todos.js
@@ -1,4 +1,28 @@
 // check off specific todos by clicking
-$("li").click(function(){
-	$(this).toggleClass("completed")
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+// click on X to delete TODO
+
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13) {
+		//pobranie tekstu z inputa
+		var todoText = $(this).val();
+		$(this).val("");
+		//stworeznie nowego li do dodania w ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText +"</li>");
+
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
 });
